8f41760d3fa34ecf0d775372338021dad3d5f838
Qian Cai <cai@lca.pw>
2019-01-31 17:47:06 -0500

    Fix a bug.

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sig=$(oc exec $1 -- for i in $(rpm -qa | grep -v gpg-pubkey); do rpm -qi $i |
-	grep ^Signature; done | cut -f3 -d"","" | sed 's/Key ID//' | tr -s ' ' |
-	cut -b10- | sort | uniq -c)
+sig=$(for i in $(rpm -qa | grep -v gpg-pubkey); do rpm -qi $i | grep ^Signature; done | cut -f3 -d"","" | sed 's/Key ID//' | tr -s ' ' | cut -b10- | sort | uniq -c)
 
 if [[ "$sig" != '443 fd431d51' ]]; then
 	echo "- error: unexpected signatures are $sig" >&2
