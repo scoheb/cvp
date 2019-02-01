fc09a4f013a742abff7338bdc081f0f9e4496a83
Qian Cai <cai@lca.pw>
2019-01-31 21:30:56 -0500

    Missing a space.

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -4,7 +4,7 @@ echo "- start: check sigatures."
 sig=$(oc exec $1 -- bash -c "rpm -qia | grep ^Signature | cut -f3 -d, | sort |
       uniq")
 
-if [[ "$sig" != 'Key ID 199e2f91fd431d51' ]]; then
+if [[ "$sig" != ' Key ID 199e2f91fd431d51' ]]; then
 	echo "- error: unexpected signatures are $sig" >&2
 	exit 1
 fi
