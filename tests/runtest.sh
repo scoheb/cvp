175b55285044f7b6847a32aeb4528a63d55af40e
Qian Cai <cai@lca.pw>
2019-01-31 20:52:20 -0500

    Try #2.

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "- start: check sigatures."
-sig=$(oc exec $1 -- bash -c "for i in $(rpm -qa | grep -v gpg-pubkey); \
-      do rpm -qi $i | grep ^Signature; done | cut -f3 -d, |
-      sort | uniq")
+sig=$(oc exec $1 -- bash -c "for i in $(rpm -qa | grep -v gpg-pubkey); do rpm -qi $i | grep ^Signature; done | cut -f3 -d, | sort | uniq")
 
 if [[ "$sig" != 'Key ID 199e2f91fd431d51' ]]; then
 	echo "- error: unexpected signatures are $sig" >&2
