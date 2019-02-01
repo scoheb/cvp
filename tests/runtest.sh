cd87d1434700ffbcfe2291fabd506dd122ecea02
Qian Cai <cai@lca.pw>
2019-01-31 20:11:51 -0500

    No runtest.sh inside the container.

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sig=$(for i in $(rpm -qa | grep -v gpg-pubkey); do rpm -qi $i | grep ^Signature; done | cut -f3 -d"","" | sed 's/Key ID//' | tr -s ' ' | cut -b10- | sort | uniq -c)
+echo "- start: check sigatures."
+sig=$(oc exec $1 -- bash -c "for i in $(rpm -qa | grep -v gpg-pubkey); \
+      do rpm -qi $i | grep ^Signature; done | cut -f3 -d"","" |
+      sed 's/Key ID//' | tr -s ' ' | cut -b10- | sort | uniq -c")
 
 if [[ "$sig" != '443 fd431d51' ]]; then
 	echo "- error: unexpected signatures are $sig" >&2
 	exit 1
 fi
-echo "- pass: image sanity tests"
+
+echo "- pass: check sigatures."
 exit 0
